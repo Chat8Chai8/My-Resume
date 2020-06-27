a4029d4cead4c858bd97830bf06c60726b54f443
ChatChai <chat8chai8@gmail.com>
2020-06-26 19:26:00 -0700

    Media added

---
+++ functions.js
@@ -0,0 +1,16 @@
+var chokIsDog = true;
+var zeldaIsCat = false;
+
+if (chokdiIsDog) {
+    console.log("Hey chokdi is a dog this is true");
+
+} else {
+    console.log("oh shit zelda is a dog");
+
+}
+
+if (zeldaIsCat) {
+    console.log("Not True");
+} else {
+    console.log("oh shit zelda is a dog");
+}
